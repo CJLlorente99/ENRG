d5d4fe6d4b9a84078e4879e4071436c411307ab0
CJLlorente99 <61738420+CJLlorente99@users.noreply.github.com>
2020-03-03 18:37:18 +0100

    Add files via upload

--- Entrega_2.m
+++ Entrega_2.m
@@ -87,3 +87,103 @@ P2final = real(Ust*I2')
 
 P1sup = real(Urt*IR1')+real(Urt*IR2')+real(Urt*IR3')
 P2sup = real(Ust*IS1')+real(Ust*IS2')+real(Ust*IS3')
+
+%% Extra
+
+w = 50;
+t = 0:1/50/40:1;
+
+% Apartado I
+
+urt = abs(Urt)*sqrt(2) * cos(w.*t + angle(Urt));
+ir1 = abs(IR1)*sqrt(2) * cos(w.*t + angle(IR1));
+
+p1 = urt.*ir1;
+
+Irc = j*IR1 * cosd(phi)*(tand(phi)-tand(phinueva));
+irc = abs(Irc)*sqrt(2) * cos(w.*t + angle(Irc));
+
+p1_corregida = urt.*(irc+ir1);
+
+figure("Name","Apartado I")
+
+subplot 221
+plot(t,p1)
+hold on;
+plot(t,ir1)
+plot(t,urt)
+legend("p_1(t)","i_{r1}(t)","u_{rt}(t)")
+grid on
+
+subplot 222
+plot(t,p1_corregida)
+hold on
+plot(t,(ir1+irc))
+plot(t,urt)
+grid on;
+legend("p_1(t) corrigiendo fdp","i_{rTotal} (i_{r1}(t)+i_{rc}(t))","u_{rt}(t)")
+
+subplot(2,2,[3 4])
+plot(t,p1)
+hold on;
+plot(t,p1_corregida)
+legend("p_1(t)","p_1(t) corrigiendo fdp")
+grid on
+
+
+% Apartado IV
+
+urt = abs(Urt)*sqrt(2) * cos(w.*t + angle(Urt));
+urs = abs(Urs)*sqrt(2) * cos(w.*t + angle(Urs));
+ust = abs(Ust)*sqrt(2) * cos(w.*t + angle(Ust));
+
+i1 = abs(I1)*sqrt(2) * cos(w.*t + angle(I1));
+i2 = abs(I2)*sqrt(2) * cos(w.*t + angle(I2));
+i3 = abs(I3)*sqrt(2) * cos(w.*t + angle(I3));
+
+ir1 = abs(IR1)*sqrt(2) * cos(w.*t + angle(IR1));
+is1 = abs(IS1)*sqrt(2) * cos(w.*t + angle(IS1));
+it1 = abs(IT1)*sqrt(2) * cos(w.*t + angle(IT1));
+
+ir2 = abs(IR2)*sqrt(2) * cos(w.*t + angle(IR2));
+is2 = abs(IS2)*sqrt(2) * cos(w.*t + angle(IS2));
+it2 = abs(IT2)*sqrt(2) * cos(w.*t + angle(IT2));
+
+ir3 = abs(IR3)*sqrt(2) * cos(w.*t + angle(IR3));
+is3 = abs(IS3)*sqrt(2) * cos(w.*t + angle(IS3));
+
+figure("Name","Apartado IV")
+
+subplot 221
+plot(t,i1.*urt)
+hold on;
+plot(t,i1)
+plot(t,urt)
+legend("p_1(t)","i_1(t)","u_{rt}(t)")
+grid on
+
+subplot 222
+plot(t,i2.*ust)
+hold on;
+plot(t,i2)
+plot(t,ust)
+legend("p_2(t)","i_2(t)","u_{st}(t)")
+grid on
+
+subplot 223
+plot(t,urt.*(ir1+ir2+ir3))
+hold on;
+plot(t,urt.*ir1)
+plot(t,urt.*ir2)
+plot(t,urt.*ir3)
+legend("p_1(t)","i_{r1}(t)·u_{rt}(t)","i_{r2}(t)·u_{rt}(t)","i_{r3}(t)·u_{rt}(t)")
+grid on
+
+subplot 224
+plot(t,ust.*(is1+is2+is3))
+hold on;
+plot(t,ust.*is1)
+plot(t,ust.*is2)
+plot(t,ust.*is3)
+legend("p_2(t)","i_{s1}(t)·u_{st}(t)","i_{s2}(t)·u_{st}(t)","i_{s3}(t)·u_{st}(t)")
+grid on
